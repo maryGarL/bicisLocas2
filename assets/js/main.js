20a54be893733781c18d612889643c83804cb04a
Mariel Garcia <maryel_565@hotmail.com>
2017-04-04 14:05:47 -0500

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,63 +4,75 @@ var email= document.getElementById('input-email');
 var pasword=document.getElementById('input-password');
 var div=document.getElementsByClassName("input-box");
 var indice = document.getElementById("form-control1");
+var span=document.getElementById("tooltiptext");
 var characterAllowed = /^[a-zA-Z]+$/;
 var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
-  function validateForm(){
-    	  	if(nombre.value.length == 0 ){
-    		tooltip("Ingresa tu Nombre",0);
-    	}
-    	else if(characterAllowed.test(nombre.value)== false){
-    		//alert("Solo vamos a aceptar letras!");
-        tooltip("Solo vamos a aceptar letras!",0);
-    	}
-    	else if(nombre.value.charAt(0) != nombre.value.charAt(0).toUpperCase()){
-    		//alert("Tu Nombre Debe comenzar con Mayúscula");
-        tooltip("Tu Nombre Debe comenzar con Mayúscula",0);
-    	}
 
+  function validateForm(){
+    	 if(nombre.value.length == 0 ){
+         mostrar("nombre","Ingrese su nombre");
+       }else {
+         ocultar("nombre")
+       }
+    	 if(characterAllowed.test(nombre.value)== false){
+    		//alert("Solo vamos a aceptar letras!");
+        mostrar("nombre","Solo vamos a aceptar letras!");
+      }else {
+        ocultar("nombre")
+      }
+    	 if(nombre.value.charAt(0) != nombre.value.charAt(0).toUpperCase()){
+    		//alert("Tu Nombre Debe comenzar con Mayúscula");
+        mostrar("nombre","Tu Nombre Debe comenzar con Mayúscula");
+      }else {
+        ocultar("nombre")
+      }
     	if(apellido.value.length == 0 ){
     		//alert("Ingresa tu apellido")
-        tooltip("Ingresa tu Apellido",1);
-    	}
-    	else if(characterAllowed.test(apellido.value)== false){
+        mostrar("apellido","Ingresa tu apellido");
+      }else {
+        ocultar("apellido")
+      }
+    	if(characterAllowed.test(apellido.value)== false){
     		//alert("Solo vamos a aceptar letras!");
-        tooltip("Solo vamos a aceptar letras!",1);
-    	}
-    	else if(apellido.value.charAt(0) != apellido.value.charAt(0).toUpperCase()){
+        mostrar("apellido","Solo vamos a aceptar letras!");
+      }else {
+        ocultar("apellido")
+      }
+    	if(apellido.value.charAt(0) != apellido.value.charAt(0).toUpperCase()){
     		//alert("Tu apellido Debe comenzar con Mayúscula");
-        tooltip("Tu apellido Debe comenzar con Mayúscula",1);
-    	}
+        mostrar("apellido","Tu apellido Debe comenzar con Mayúscula");
+      }else {
+        ocultar("apellido")
+      }
 
     	if((email.value=="")||(emailRegex.test(email.value)==false)){
     		//alert("Ingrese un email valido");
-        tooltip("Ingrese un email valido",2);
-    	};
+        mostrar("correo","Ingrese un email valido");
+      }else {
+        ocultar("correo")
+      }
 
     	if((pasword.value=="")||(pasword.value.length>=7)||( pasword.value=="password")||(pasword.value=="123456")||(pasword.value=="098754")){
     		//alert("Ingreso correctamente su contraseña");
-        tooltip("Ingreso correctamente su contraseña",3);
-      } else {
-      removetooltip(3);
+        mostrar("contra","Ingreso correctamente su contraseña");
+      }else {
+        ocultar("contra")
       }
 
     if( indice.selectedIndex == null || indice.selectedIndex == 0 ) {
       //alert("Seleccione una opcion");
-      tooltip("Seleccione una opcion",4);
+      mostrar("selectvo","Seleccione una opcion");
     }else {
-      removetooltip(4);
+      ocultar("selectvo")
     }
 }
-
-function tooltip(e,index){
-  var t=document.createElement("span");
-  t.setAttribute("class", "tooltiptext" );
-  t.appendChild(document.createTextNode(e));
-  div[index].appendChild(t);
-};
-function removetooltip(index){
-    var delette=document.getElementsByClassName("tooltiptext")[index];
-  div[index].removeChild(delette);
-
-  }
+function mostrar(id,message){
+ var x=document.getElementById(id);
+ x.innerHTML=message;
+ x.style.display= "block";
+}
+function ocultar(id){
+var x=document.getElementById(id)
+x.style.display= "none";
+}
